1db135610c7b540108dd0607e7a173222cab209f
jaycammarano <jaycammarano@gmail.com>
2021-06-12 17:40:49 -0400

    artist page started. still needs routes

--- client/src/components/ArtistPage.tsx
+++ client/src/components/ArtistPage.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const ArtistPage = () => {
-    return (
-        <div>
-            
+const ArtistPage: React.FC = () => {
+  // fetch name and releases
+  // lazy load bio from Last.FM +/or discogs
+  // lazy load similar artists based on tags?
+  return (
+    <div className="text-white ">
+      <div className="m-2 font-bold text-white">
+        <div className="m-4 text-7xl">Artist Name</div>
+        <div className="flex flex-row">
+          <div className="w-1/3 px-8 py-3 pr-32 m-2 text-3xl bg-green-500 border-4 border-green-500 rounded focus:border-green-300 hover:border-green-300 hover:border-2">
+            Releases
+          </div>
+          <div className="w-1/3 px-8 py-3 pr-32 m-2 text-3xl bg-green-500 border-4 border-green-500 rounded hover:border-green-300 hover:border-2">
+            Bio
+          </div>
+          <div className="w-1/3 px-8 py-3 pr-32 m-2 text-3xl bg-green-500 border-4 border-green-500 rounded hover:border-green-300 hover:border-2">
+            Related
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ArtistPage
+export default ArtistPage;
---
+++ client/src/components/lib/types.d.ts
@@ -0,0 +1,5 @@
+export type Release = {
+    title: string
+    tags: string[]
+    cover: string
+}

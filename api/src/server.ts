3da46cf839651e191754d84481462c9458b562ea
jaycammarano <jaycammarano@gmail.com>
2021-06-13 22:49:48 -0400

    formatting

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,45 +1,54 @@
-import 'reflect-metadata'
-import {ApolloServer} from "apollo-server-express"
-import Express from "express"
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import Express from "express";
+import { buildSchema } from "type-graphql";
 import {
-  buildSchema,
-
-} from "type-graphql";
-import {
-    ArtistsOnReleasesRelationsResolver,
-    ArtistsOnReleasesCrudResolver,
-    ArtistRelationsResolver,
-    ReleaseRelationsResolver,
-    ArtistCrudResolver,
-    ReleaseCrudResolver,
-} from "../prisma/generated/type-graphql"
+  ArtistsOnReleasesRelationsResolver,
+  ArtistsOnReleasesCrudResolver,
+  ArtistRelationsResolver,
+  ReleaseRelationsResolver,
+  ArtistCrudResolver,
+  ReleaseCrudResolver,
+} from "../prisma/generated/type-graphql";
 import path from "path";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-import { LastFMResolver } from './GraphQL/lastFm';
+import { LastFMResolver } from "./GraphQL/lastFm";
 
 interface Context {
-    prisma: PrismaClient
+  prisma: PrismaClient;
 }
 
 const main = async () => {
-    const schema = await buildSchema({
-        resolvers: [ArtistRelationsResolver, ReleaseCrudResolver, ArtistCrudResolver, ReleaseRelationsResolver, ArtistsOnReleasesRelationsResolver, ArtistsOnReleasesCrudResolver, LastFMResolver],
-        emitSchemaFile: path.resolve(__dirname, "./GraphQL/generated-schema.graphql"),
-        validate: false,
-    })
-    const prisma = new PrismaClient();
+  const schema = await buildSchema({
+    resolvers: [
+      ArtistRelationsResolver,
+      ReleaseCrudResolver,
+      ArtistCrudResolver,
+      ReleaseRelationsResolver,
+      ArtistsOnReleasesRelationsResolver,
+      ArtistsOnReleasesCrudResolver,
+      LastFMResolver,
+    ],
+    emitSchemaFile: path.resolve(
+      __dirname,
+      "./GraphQL/generated-schema.graphql"
+    ),
+    validate: false,
+  });
+  const prisma = new PrismaClient();
 
-    const apolloSever = new ApolloServer({
-        schema,
-        playground: true,
-        context: (): Context => ({ prisma })})
-    const app = Express()
+  const apolloSever = new ApolloServer({
+    schema,
+    playground: true,
+    context: (): Context => ({ prisma }),
+  });
+  const app = Express();
 
-    apolloSever.applyMiddleware({app})
-    app.listen(4000, () => {
-        console.log('server started on http://localhost:4000/graphql')
-    })
-}
+  apolloSever.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
+};
 
-main()
+main();
